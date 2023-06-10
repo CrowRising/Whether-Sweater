b592f8657177ab79c96658dffbdb8413ca6d3f4d
Crow Rising <miscbyrd@gmail.com>
2023-06-10 17:23:36 -0600

    test:complete test for map service

--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe MapService do
-  describe 'instance methods' do
-    it 'get_map', :vcr do
+  describe 'connection exists' do
+    it 'it can connect and get map', :vcr do
       map_service = MapService.new
-      map_results = map_service.get_map('Denver')
+      map_results = map_service.get_map('Denver, CO')
 
-      expect(map_results).to be a Hash
-      expect(map_results).to have_key(:results)
-      expect(map_results[:results]).to be_an Array 
-      expect(map_results[:results]).to have_key(:)
-      expect(map_results[:results][:])
+      expect(map_results).to be_a Hash
+      expect(map_results).to have_key :results
+      expect(map_results[:results][0][:locations][0][:latLng]).to have_key :lat
+      expect(map_results[:results][0][:locations][0][:latLng]).to have_key :lng 
+      expect(map_results[:results][0][:locations][0][:latLng][:lat]).to be_a Float
+      expect(map_results[:results][0][:locations][0][:latLng][:lng]).to be_a Float
+      
     end
   end
 end
