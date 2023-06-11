3a08ad41cafe1ea7d654a822d8025995c445b0a2
Crow Rising <miscbyrd@gmail.com>
2023-06-11 16:57:39 -0600

    test: add testing for request data types

--- spec/requests/api/v1/forecast_data_request_spec.rb
+++ spec/requests/api/v1/forecast_data_request_spec.rb
@@ -6,6 +6,24 @@ RSpec.describe 'ForecastData', type: :request do
       get '/api/v1/forecast?location=denver,co'
 
       expect(response).to have_http_status :success
+
+      weather_stats = JSON.parse(response.body, symbolize_names: true)
+
+      expect(weather_stats).to be_a Hash
+      expect(weather_stats).to have_key :data
+      expect(weather_stats[:data]).to be_a Hash
+      expect(weather_stats[:data]).to have_key :id
+      expect(weather_stats[:data][:id]).to eq(nil)
+      expect(weather_stats[:data]).to have_key :type
+      expect(weather_stats[:data][:type]).to eq('forecast_data')
+      expect(weather_stats[:data]).to have_key :attributes
+      expect(weather_stats[:data][:attributes]).to be_a Hash
+      expect(weather_stats[:data][:attributes]).to have_key :current_weather
+      expect(weather_stats[:data][:attributes]).to have_key :hourly_weather
+      expect(weather_stats[:data][:attributes]).to have_key :daily_weather
+
+
+
     end
   end
 end
