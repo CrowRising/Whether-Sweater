4c7cb536bd5fe5baff5d82d0936e22963088f66b
Crow Rising <miscbyrd@gmail.com>
2023-06-11 11:00:34 -0600

    chore: make route to forecast data with index action

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api do
+    namespace :v1 do
+      get '/forecast', to: 'forecast_data#index'
+    end
+  end
 end
