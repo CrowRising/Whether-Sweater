52d61cf03abfe74d033121b38e8bbf5cedd1330e
Crow Rising <miscbyrd@gmail.com>
2023-06-12 10:03:48 -0600

    feat: add book facade

---
+++ app/facades/book_facade.rb
@@ -0,0 +1,19 @@
+class BookFacade
+  def initialize(book_data)
+    @book_data = book_data
+  end
+
+  def total_books_found
+    @book_data[:num_found]
+  end
+
+  def books
+    @book_data[:docs].map do |book_info|
+      {
+        title: book_info[:title],
+        publisher: book_info[:publisher].first,
+        isbn: book_info[:isbn]
+      }
+    end
+  end
+end
