60801414fadc10335aafff0a07c0774e51e6d11e
Crow Rising <miscbyrd@gmail.com>
2023-06-12 11:03:24 -0600

    chore: add books to weather poros

--- app/poros/weather.rb
+++ app/poros/weather.rb
@@ -4,12 +4,14 @@ class Weather
   attr_reader :id,
               :current_weather,
               :daily_weather,
-              :hourly_weather
+              :hourly_weather,
+              :books
 
   def initialize(data)
     @id = nil
     @current_weather = data[:current_weather]
     @daily_weather = data[:daily_weather]
     @hourly_weather = data[:hourly_weather]
+    @books = data[:books]
   end
 end
