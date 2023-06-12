2ba19714ec59416018d5ab09fd25334b55aac41e
Crow Rising <miscbyrd@gmail.com>
2023-06-12 11:15:48 -0600

    test: first tests for successful connection

--- spec/requests/api/v1/book_search_spec.rb
+++ spec/requests/api/v1/book_search_spec.rb
@@ -1,1 +1,12 @@
-require
+require 'rails_helper'
+
+RSpec.describe 'Book Search API', type: :request do
+  describe 'Search for books by location and quantity' do
+    it 'returns book data', :vcr do
+      get '/api/v1/book-search?location=denver,co&quantity=5'
+
+      expect(response).to have_http_status :success
+      expect(response.status).to eq(200)
+    end
+  end
+end
