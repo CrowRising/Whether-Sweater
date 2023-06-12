c1591eca06fa9dfe3c6e04bf8c5af1e37bb7d10c
Crow Rising <miscbyrd@gmail.com>
2023-06-12 11:16:09 -0600

    chore: add route for book_search

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/forecast', to: 'forecast_data#index'
+      get '/book-search', to: 'book_search#index'
     end
   end
 end
