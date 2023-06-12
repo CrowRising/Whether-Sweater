486c01ae67c1504f556e8ff2cede524793133efc
Crow Rising <miscbyrd@gmail.com>
2023-06-12 09:39:22 -0600

    test: add test for book service

--- spec/services/book_service_spec.rb
+++ spec/services/book_service_spec.rb
@@ -10,7 +10,19 @@ RSpec.describe 'Book Search API' do
       expect(book_results).to have_key :docs
       expect(book_results[:docs]).to be_an Array
       
-      
+      book = book_results[:docs].first
+
+      expect(book).to have_key :title
+      expect(book[:title]).to be_a String
+
+      expect(book).to have_key :publisher
+      expect(book[:publisher]).to be_an Array
+
+      expect(book).to have_key :isbn
+      expect(book[:isbn]).to be_an Array
+
+      expect(book_results).to have_key :numFound
+      expect(book_results[:numFound]).to be_an Integer
     end
   end
 end
