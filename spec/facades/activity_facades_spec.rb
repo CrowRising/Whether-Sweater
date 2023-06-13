0332263827d50e7e6efc2fc165a491fbd5bfb476
Crow Rising <miscbyrd@gmail.com>
2023-06-13 10:07:34 -0600

    test: add test for facade

---
+++ spec/facades/activity_facades_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe 'ActivityFacade' do
+  describe 'ActivityFacade methods', :vcr do
+    let(:activity_facade) { ActivityFacade.new('recreational').get_activity_info }
+    it 'returns an activity' do
+      # require 'pry'; binding.pry
+      expect(activity_facade).to be_a Hash
+      expect(activity_facade.activity).to be_an Array
+    end
+  end
+end
